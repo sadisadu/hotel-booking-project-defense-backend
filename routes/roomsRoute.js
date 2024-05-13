60e6afbe456a5af1e3ae21928884b2768f907d71
Mohammad Mahabub Alam <mahabubalam3004@gmail.com>
2024-05-13 18:38:44 +0600

    all user all rooms all bookings done

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -3,26 +3,32 @@ const router = express.Router();
 
 const Room = require('../models/rooms')
 
-router.get("/getallrooms",async(req,res) => {
+router.get("/getallrooms", async (req, res) => {
 
-    try{
+    try {
         const rooms = await Room.find({})
         res.send(rooms)
-    }catch (error){
-        return res.status(400).json({message: error});
+    } catch (error) {
+        console.error("Error occurred during all rooms:", error);
+        res
+            .status(500)
+            .send("An error occurred during all rooms. Please try again later.");
     }
 });
 
 
-router.post("/getroombyid",async(req,res) => {
+router.post("/getroombyid", async (req, res) => {
     const roomid = req.body.roomid;
-    try{
-        const room = await Room.findOne ({_id : roomid});
+    try {
+        const room = await Room.findOne({ _id: roomid });
         res.send(room);
-    }catch (error){
-        return res.status(400).json({message: error});
+    } catch (error) {
+        console.error("Error occurred during roombyid:", error);
+        res
+            .status(500)
+            .send("An error occurred during roombuid. Please try again later.");
     }
 });
 
-module.exports=router;
+module.exports = router;
 
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,42 +1,56 @@
-const express =require("express");
+const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js")
 
-router.post("/register",async(req,res)=>{
+router.post("/register", async (req, res) => {
     // console.log("i am new user ", newuser)
-    
-    try{
-        const newuser =new User({name:req.body.name,email:req.body.email,password:req.body.password})
+
+    try {
+        const newuser = new User({ name: req.body.name, email: req.body.email, password: req.body.password })
         await newuser.save()
         res.status(200).send('User Registered Successfully')
 
-    }catch(error){
-        return res.status(400).json({error});
+    } catch (error) {
+        return res.status(400).json({ error });
     }
 });
 
-router.post("/login",async(req,res)=> {
+router.post("/login", async (req, res) => {
 
-    const {email,password}=req.body
+    const { email, password } = req.body
 
-    try{
-        const user =await User.findOne({email:email,password:password})
-        if(user){
+    try {
+        const user = await User.findOne({ email: email, password: password })
+        if (user) {
             const temp = {
-                name:user.name,
-                email:user.email,
+                name: user.name,
+                email: user.email,
                 isAdmin: user.isAdmin,
-                _id:user._id,
+                _id: user._id,
             }
             res.send(user)
         }
-        else{
-            return res.status(400).json({message:'Login failed'});
+        else {
+            return res.status(400).json({ message: 'Login failed' });
         }
-    }catch(error){
+    } catch (error) {
         console.log(error)
-        return res.status(400).json({error});
+        return res.status(400).json({ error });
     }
 });
 
-module.exports=router
+// get all users
+router.get("/getallusers", async (req, res) => {
+
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        console.error("Error occurred during all users:", error);
+        res
+            .status(500)
+            .send("An error occurred during all users. Please try again later.");
+    }
+});
+
+module.exports = router
